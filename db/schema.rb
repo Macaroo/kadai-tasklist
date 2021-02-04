44fa9dbb7ed0844d4ca23717756b49eff1032b54
manaron <kmacaro67j2@gmail.com>
2021-02-04 06:51:34 +0000

    task model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_03_064412) do
+ActiveRecord::Schema.define(version: 2021_02_04_045422) do
 
-  create_table "messages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
